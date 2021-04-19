2695ba61e27fe9419d24897cee0ce00a17b9dcd9
Priyanka Deshmukh <priyankadeshmukh070@gmail.com>
2021-04-20 00:38:23 +0530

    GFG - leaders in array.

---
+++ leadersInArray.js
@@ -0,0 +1,42 @@
+/**
+ * Problem Statement:
+ * Given an array A of positive integers. Your task is to find the leaders in the array. 
+ * An element of array is leader if it is greater than or equal to all the elements
+ * to its right side. The rightmost element is always a leader.
+ * 
+ * Example 1:
+    * Input:
+        * n = 6
+        * A[] = {16,17,4,3,5,2}
+    * Output: 17 5 2
+    * Explanation: The first leader is 17 as it is greater than all the elements to its 
+        * right. Similarly, the next leader is 5. The right most element is always a leader 
+        * so it is also included.
+ * Example 2:
+    * Input:
+        * n = 5
+        * A[] = {1,2,3,4,0}
+    * Output: 4 0
+
+
+ */
+
+function leaders(arr, n){
+    // code here
+    let max = arr[n-1];
+
+    let op = [];
+    op[0] = max;
+
+    for(let i=n-2;i>=0;i--) {
+        if(arr[i] >= max) {
+            max = arr[i];
+            op.push(arr[i]); // unshift can also be used here. But it is giving timeout error.
+        }
+    }
+    
+    return op.reverse();
+    
+}
+
+console.log(leaders([16,17,4,3,5,2], 6));
