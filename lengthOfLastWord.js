0873952cb4ebdb5be26440ab5ff804b0f4f86d97
Priyanka Deshmukh <priyankadeshmukh070@gmail.com>
2022-02-28 17:09:59 +0530

    minor fix

--- lengthOfLastWord.js
+++ lengthOfLastWord.js
@@ -3,4 +3,6 @@
 var lengthOfLastWord = function(s) {
     // s.match(/\b(\w+)\b/g).slice(-1)[0].length also returns similar result. but slower than pop.
     return s.match(/\b(\w+)\b/g).pop().length;
-}
+};
+
+lengthOfLastWord("Today is My day    "); // last word day so, length of last word is 3.
